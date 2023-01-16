d87d09ed12933ad60e8cfab0a3da8ac5ed264945
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-01-16 15:04:11 +0100

    Completata MILESTONE 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,42 @@
 // array
 const members = [
 
-    // memebro 1 - indice 0 
+    // membro 1 - indice 0 
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
         img: 'wayne-barnett-founder-ceo.jpg'
     },
 
-    // memebro 2 - indice 1
+    // membro 2 - indice 1
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
         img: 'angela-caroll-chief-editor.jpg'
     },
 
-    // memebro 3 - indice 2
+    // membro 3 - indice 2
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
         img: 'walter-gordon-office-manager.jpg'
     },
 
-    // memebro 4 - indice 3
+    // membro 4 - indice 3
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
         img: 'angela-lopez-social-media-manager.jpg'
     },
 
-    // memebro 5 - indice 4
+    // membro 5 - indice 4
     {
         name: 'Scott Estrada',
         role: 'Developer',
         img: 'scott-estrada-developer.jpg'
     },
 
-    // memebro 6 - indice 5
+    // membro 6 - indice 5
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
@@ -45,12 +45,46 @@ const members = [
 
 ];
 
+const myContainer = document.getElementById('container');
+
+let myUl = document.createElement('ul');
+
+myContainer.append(myUl);
+
 for (let i = 0; i < members.length; i++) {
 
+    // stampa in console
     console.log('Nome:' + " " + members[i].name);
     console.log('Ruolo:' + " " + members[i].role);
     console.log('Immagine:' + " " + members[i].img);
 
-    console.log('-------------')
+    console.log('-------------');
 
-}
+    // stampa nel DOM
+    let myLi = document.createElement('li');
+
+    myUl.append(myLi);
+
+    let myPName = document.createElement('p');
+
+    myLi.append(myPName);
+
+    myPName.innerHTML = 'Nome:' + " " + members[i].name; // stampa nel DOM il nome
+
+    let myPRole = document.createElement('p');
+
+    myLi.append(myPRole);
+
+    myPRole.innerHTML = 'Ruolo:' + " " + members[i].role; // stampa nel DOM il ruolo
+
+    let myPImg = document.createElement('p');
+
+    myLi.append(myPImg);
+
+    myPImg.innerHTML = 'Img:' + " " + members[i].img; // stampa nel DOM l'immagine
+
+    let myHr = document.createElement('hr');
+
+    myLi.append(myHr);
+
+};
